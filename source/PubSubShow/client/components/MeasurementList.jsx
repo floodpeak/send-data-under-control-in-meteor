12b3a10d99eab100c3be0154ad17dd5b10185297
floodpeak <hongfengzhou@yahoo.com>
2016-03-06 01:18:39 +0800

    add page up to sub lifecycle

--- source/PubSubShow/client/components/MeasurementList.jsx
+++ source/PubSubShow/client/components/MeasurementList.jsx
@@ -7,11 +7,13 @@ MeasurementList = React.createClass({
       measurements = Measurements.find({userId:Meteor.userId()}).fetch()
     }
     return {
+      sub:sub,
       measurements:measurements
     }
   },
   render(){
     return <div>
+      <button type="button" onClick={this.data.sub.stop}>stop subscribe</button>
       {
         this.data.measurements.map((item,index)=>{
           return <MeasurementItem {...item} key={index} />
